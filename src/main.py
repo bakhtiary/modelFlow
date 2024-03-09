127b64e7b176ac93443f06abf44dadf75a07975c
Negar <n.zakerinejad@gmail.com>
2024-03-09 21:52:17 +0100

    adding authentication

--- src/main.py
+++ src/main.py
@@ -1,11 +1,60 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi import APIRouter, Depends
+from fastapi import Security, HTTPException, status
+from fastapi.security import APIKeyHeader
 
 app = FastAPI()
+public_router = APIRouter()
 
+api_key_header = APIKeyHeader(name="X-API-Key")
+
+
+def get_user(api_key_header: str = Security(api_key_header)):
+    if api_key_header == "hasankey":
+        user = "hasan"
+        return user
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Missing or invalid API key"
+    )
+
+@public_router.get("/")
+async def get_testroute():
+    return "OK"
+app.include_router(
+    public_router,
+    prefix="/api/v1/public"
+)
+
+secure_router = APIRouter()
+
+@secure_router.get("/")
+async def get_testroute(user: dict = Depends(get_user)):
+    return user
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-@app.get("/classifyImage")
+
+
+@secure_router.get("/classifyImage")
 async def classifyImage():
+
     return {"cat"}
+
+
+app.include_router(
+    secure_router,
+    prefix="/api/v1/secure",
+    dependencies=[Depends(get_user)]
+)
+# async def classifyImage(file: UploadFile = File()):
+#     try:
+#         contents = await file.read()
+#
+#     except Exception:
+#         return {"message": "There was an error uploading the file"}
+#     finally:
+#         await file.close()
+#
+#     return {"message": f"Successfuly uploaded {file.filename}"}
